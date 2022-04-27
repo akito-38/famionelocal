c932fa4d548fff3f16a6a9c27b1cda2e51eb8086
miyamoto <m.akirabo@gmail.com>
2022-04-27 18:53:30 +0900

    詳細作成

--- wp/app/public/wp-content/themes/benefit_v01/single-interview.php
+++ wp/app/public/wp-content/themes/benefit_v01/single-interview.php
@@ -2,7 +2,7 @@
 <!-- ファーストビュー -->
 <div class="fv-list">
     <div class="fv-inner-list">
-        <h2>導入・提供事例</h2>
+        <h2>取組実績</h2>
         <p class="fv-pc"><?php the_field('company'); ?></p>
     </div>
 </div>
@@ -14,9 +14,9 @@
         <div class="main-box">
             <h1><?php the_title(); ?></h1>
             <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'large'); ?>" alt="">
-            <p><?php the_field('interviewee'); ?></p>
+            <p class="interviewee"><?php the_field('interviewee'); ?></p>
         </div>
-        <div class="main-box">
+        <div class="main-box ">
 
             <?php the_content(); ?>
 
@@ -33,13 +33,13 @@
             ?>
             <img src="<?php echo $src; ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>" alt="">
             <div class="company-data-box">
-                <p>社名<br>
+                <p><strong>社名</strong><br>
                     <?php the_field('company'); ?>
                 </p>
-                <p>業種<br>
+                <p><strong>業種</strong><br>
                     <?php the_field('industry'); ?>
                 </p>
-                <p>従業員数<br>
+                <p><strong>従業員数</strong><br>
                     <?php the_field('employee_num'); ?>
                 </p>
             </div>
@@ -50,22 +50,29 @@
             </div>
         </div>
         <div class="topics-sub-box">
-            <h2>ファミワンの妊活・不妊治療<br>福利厚生サポート</h2>
+            <h2 class="topics-sub-box-txt">ファミワンが住民の皆さまの<span class="orange">妊活・不妊治療の相談</span>をフルサポートします</h2>
             <div class="btn-box btn-column">
-                <div class="btn btn-contact btn-column-item"><a href="https://famione.co.jp/benefit-contact/">お問い合わせ</a>
+                <div class="success-list-btn btn"><a href="https://famione.co.jp/benefit-contact/">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/top/email.svg" alt=""
+                            width="20" loading="lazy">
+                        お問い合わせ
+                    </a>
                 </div>
-                <div class="btn btn-data btn-column-item"><a href="https://famione.co.jp/benefit-download/">資料請求</a>
+                <div class="border-list-btn btn"><a href="https://famione.co.jp/benefit-download/">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/top/download.svg" alt=""
+                            width="20" loading="lazy">
+                        資料請求</a>
                 </div>
             </div>
         </div>
     </div>
     <!-- ボタンを横並びにするために親要素を追加 -->
     <div class="main-btn-box">
-        <div class="success-list-btn btn main-btn">
+        <div class="border-list-btn btn">
             <a href="<?php echo home_url(); ?>">TOPに戻る</a>
         </div>
-        <div class="success-list-btn btn main-btn">
-            <a href="<?php echo home_url(); ?>/interview/">事例一覧</a>
+        <div class="border-list-btn btn">
+            <a href="<?php echo home_url(); ?>/interview/">導入事例一覧</a>
         </div>
     </div>
 
